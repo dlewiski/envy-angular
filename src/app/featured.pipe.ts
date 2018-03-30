ca2adf47adea25ab60cdaceca8ff5f8d6ac21005
David Lewis <davidlewisp@gmail.com>
2018-03-30 15:07:18 -0700

    fixed pipe funcionality by adding it to the NgModule declarations

--- src/app/featured.pipe.ts
+++ src/app/featured.pipe.ts
@@ -9,6 +9,7 @@ import { GreenStory } from './models/greenStory.model';
 export class FeaturedPipe implements PipeTransform {
   transform(input: GreenStory[], args) {
     let output: GreenStory[] = [];
+    console.log("here");
     for (let i = 0; i < input.length; i++) {
       if (input[i].featured === true) {
         output.push(input[i]);
