3eb912217086ff96104dd38b7ad955b282b69ca4
David Lewis <davidlewisp@gmail.com>
2018-03-30 16:18:14 -0700

    added environmental impact level in nav bar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -16,7 +16,9 @@ export class AppComponent {
   stories: GreenStory[] = [
   new GreenStory('You Can Fish Plastic From Amsterdams Canals On This Eco-Boat Tour','The company operates a fleet of ten boats made from recovered plastic.', 'DESIRÉE KAPLAN', 'waste', 3, 'http://www.greenmatters.com/community/2018/03/30/ZhFkv6/plastic-amsterdam-canals', 'http://media.greenmatters.com/Z2aQO7Y/770x404/pexels-photo-851039-1522433681590.jpeg', true),
 
-  new GreenStory('Adiddas Recyled Yoga Line','Adidas has committed to making a dent in plastic pollution by teaming up with Parley for the Oceans to create products out of recycled plastic.', 'BRIAN SPAEN', 'clothing', 24, 'http://www.greenmatters.com/news/2018/03/29/Z1yaU1T/adidas-yoga', 'http://media.greenmatters.com/1BrB7o/970x508/screenshot-twitter.com-2018.03.30-08-58-53-1522414745627.png', false)
+  new GreenStory('Adiddas Recyled Yoga Line','Adidas has committed to making a dent in plastic pollution by teaming up with Parley for the Oceans to create products out of recycled plastic.', 'BRIAN SPAEN', 'clothing', 24, 'http://www.greenmatters.com/news/2018/03/29/Z1yaU1T/adidas-yoga', 'http://media.greenmatters.com/1BrB7o/970x508/screenshot-twitter.com-2018.03.30-08-58-53-1522414745627.png', false),
+
+  new GreenStory('This Device Can Double The "Green" Life Of Bananas To Reduce Waste','Have you ever had your bananas go brown while sitting on your kitchen counter? Sadly, youre not alone. Half of the produce we buy on average is thrown away, and bananas are one of the most popular in that group. In order to extend the life of these bananas and other fresh produce, one company has created a filter that prevents them from ripening.', 'BRIAN SPAEN', 'food', 21, 'http://www.greenmatters.com/food/2018/03/30/Z2wBmsE/produce-double-life-new-filter', 'http://media.greenmatters.com/Z1LXbpa/840x440/cereal-breakfast-meal-food-1522430846897.jpg', false)
 ];
 
   toolkit: GreenTool[] = [
