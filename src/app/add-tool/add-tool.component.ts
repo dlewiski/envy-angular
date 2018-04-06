2180cf573d99474241352b4381e5e42305433756
David Lewis <davidlewisp@gmail.com>
2018-04-06 12:11:10 -0700

    succesfully add a tool to the database

--- src/app/add-tool/add-tool.component.ts
+++ src/app/add-tool/add-tool.component.ts
@@ -1,15 +1,24 @@
 import { Component, OnInit } from '@angular/core';
+import { GreenTool } from '../models/greenTool.model';
+import { ToolkitService} from '../toolkit.service';
 
 @Component({
   selector: 'app-add-tool',
   templateUrl: './add-tool.component.html',
-  styleUrls: ['./add-tool.component.css']
+  styleUrls: ['./add-tool.component.css'],
+  providers: [ToolkitService]
 })
 export class AddToolComponent implements OnInit {
 
-  constructor() { }
+  constructor(private toolkitService: ToolkitService) { }
 
   ngOnInit() {
+  }
+
+  createTool(name:string, description:string, effectiveness:number) {
+    let newTool: GreenTool = new GreenTool(name, description, effectiveness);
+    this.toolkitService.addTool(newTool);
+    console.log(newTool);
   }
 
 }
