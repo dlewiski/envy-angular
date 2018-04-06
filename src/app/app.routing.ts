c12f781f83bf20a3cb1f27d750176cf1f414ae3a
David Lewis <davidlewisp@gmail.com>
2018-04-06 14:42:36 -0700

    add score card route to environmental score

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -3,6 +3,8 @@ import { Routes, RouterModule } from '@angular/router';
 import { StoryDisplayComponent } from './story-display/story-display.component';
 import { GreenToolkitComponent } from './green-toolkit/green-toolkit.component';
 import { AddToolComponent } from './add-tool/add-tool.component';
+import { AddStoryComponent } from './add-story/add-story.component';
+import { GreenScoreComponent } from './green-score/green-score.component';
 
 const appRoutes: Routes = [
   {
@@ -16,7 +18,16 @@ const appRoutes: Routes = [
   {
     path:'toolkit/addtool',
     component: AddToolComponent
+  },
+  {
+    path:"addstory",
+    component: AddStoryComponent
+  },
+  {
+    path:"scorecard",
+    component: GreenScoreComponent
   }
+
 ];
 
 export const routing: ModuleWithProviders = RouterModule.forRoot(appRoutes);
